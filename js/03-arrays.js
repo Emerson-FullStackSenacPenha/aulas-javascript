9bf5a1a511e4fbd238fb07855ca41f157e1763bd
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-07-30 17:27:15 -0300

    Acessando Arrays

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,4 +1,17 @@
 'use strict';
 
-console.log("Ola");
+// Declarar uma array (vetor)
+let vocalistas = ["Ozzy", "Dio", "Lemmy", "David Gilmour"];
+
+// Se quisermos visualizar a estrutura do array interio:
+// console.log(vocalistas);
+
+// Acessando um determinado elemento através do índice
+console.log(vocalistas[0]);
+
+// O David Gilmour está em turnê tocando Pink Floyd
+console.log(`O ${vocalistas[3]} está em turnê tocando Pink Floyd`);
+
+
+
 
