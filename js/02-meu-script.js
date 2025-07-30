376bc0759a7eafb15be99314ec959829d38afd7d
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-07-30 15:24:05 -0300

    exercicio de contatenar

--- js/02-meu-script.js
+++ js/02-meu-script.js
@@ -1,1 +1,14 @@
-console.log("Olá");
+/*Exercícios*/
+
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+
+// 2) Crie uma varável chamada "competência" e coloque nela uma frase que representa o que estamos estudando nesta UC.
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+
+let uni = "Penha"
+let uc = 2
+let competencia = "Front-End com JavaScript"
+
+console.log(`Estamos no Senac ${uni}, na UC${uc} estudando ${competencia}.`);
+
