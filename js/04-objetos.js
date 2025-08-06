860562ea21b37ebdab6ca6c1588bcd40bb8fbf2f
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-06 16:31:34 -0300

    Objeto Simples

---
+++ js/04-objetos.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Exemplo 1: Objeto com dados de uma pessoa
+
+const pessoa = {
+     
+    // propriedado: valor
+    nome: "Chaves", 
+    idade: 8,
+    cidade: "São Paulo",
+    estado: "SP"
+
+};
+
+console.log(pessoa);
+
+// Acessamos as propriedades usando nomeDoObjeto.nomeDaPropriedade
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos. `);
+
