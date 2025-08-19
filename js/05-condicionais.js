b85254886479afdc93c62d181b1dc1fe5eb9ec00
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-19 14:36:21 -0300

    Condicional simples apenas com if

---
+++ js/05-condicionais.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Comandos condicionais mais comuns: 
+if   ->  SE
+else -> SENÃO
+*/
+
+console.log("Emxeplo 1");
+
+let numero = 50;
+
+
+// Condicional SIMPLES (Usa apenas if)
+
+if(numero >= 10){
+
+    console.log("Se você está vendo essa mensagem, é porque a condição é VERDADEIRA!");
+    
+
+}
