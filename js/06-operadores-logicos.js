f4e4134581d19559bbb3533a8ebff3f32820b246
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-19 16:51:54 -0300

    Igual duplo ou triplo

---
+++ js/06-operadores-logicos.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*
+Operadores LÒGICOS
+
+&&  -> E            -> AND
+||  -> OU           -> OR
+!   -> NÂO/NEGAÇÃO  -> NOT
+*/
+
+console.log("Exemplo &&");
+let idade = 16;
+let temCarta = true;
+
+if (idade >= 18 && temCarta === true) {
+    console.log("Pode dirigir");
+} else{
+    console.log("Não pode dirigir");
+}
+
+/* Sobre duplo e triplo sinal de igual */
+
+let a = 10;
+let b = '10';
+let resultadoComparacao = a === b; //False
+// let resultadoComparacao = a == b; True
+console.log(resultadoComparacao);
+
+/* Sobre duplo e triplo sinal de igual
+
+Ao usar == a comparação é domente do VALOR, ou seja, não importa o tipo de dados (String, Number, Etc)
+
+Ao usar === a comparação é do VALOR e do TIPO de dado, ou seja, importa tudo. */
