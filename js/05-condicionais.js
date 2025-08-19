4ab0b953078bc9def7866a37d731be21420a5204
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-19 14:56:05 -0300

    condição composta

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -12,9 +12,25 @@ let numero = 50;
 
 // Condicional SIMPLES (Usa apenas if)
 
-if(numero >= 10){
+if (numero >= 10) {
 
     console.log("Se você está vendo essa mensagem, é porque a condição é VERDADEIRA!");
-    
 
 }
+
+console.log("\nExemplo 2");
+
+let aluno = "Ozzy"
+let idade = 30;
+
+/* Lógica: Verificar se o aluno é maior ou menor de idade. */
+if (idade < 18) {
+    console.log("Você é menor de idade!");
+} else {
+    console.log("Você é maior de idade!");
+}
+
+
+
+
+
