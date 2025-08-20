f3cbac152ee5f0c7b46e2ed0293af7699e8e1119
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-20 14:11:39 -0300

    contador While

--- js/07-loops.js
+++ js/07-loops.js
@@ -7,3 +7,12 @@ Comandos mais tradicionais: while (Enquanto) e for (Para)
 
 Normalmente, os loops são controlados através de uma variável contadora.
 */
+console.log("Exemplo 1: WHILE (enquanto)");
+
+let contador = 1;
+while(contador <= 5){
+    console.log("Valor do contator: + "+contador);
+    contador++; // incremento
+    // contador = contador + 1 (seria a mesma coisa)
+}
+
