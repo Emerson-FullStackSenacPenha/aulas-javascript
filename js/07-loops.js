8eb24f18f848206044da0cc23557d4e18633f5bc
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-20 14:26:03 -0300

    looping For

--- js/07-loops.js
+++ js/07-loops.js
@@ -6,13 +6,25 @@ Loops ou estruturas de repetição são comandos que, baseados em alguma condiç
 Comandos mais tradicionais: while (Enquanto) e for (Para)
 
 Normalmente, os loops são controlados através de uma variável contadora.
+
+Está variável "contador" é chamada de "i", "j", "k" ou qualquer outra letra se necessário.
+
+"Cont", quando é relacionado a dados
+
 */
 console.log("Exemplo 1: WHILE (enquanto)");
 
-let contador = 1;
-while(contador <= 5){
-    console.log("Valor do contator: + "+contador);
-    contador++; // incremento
-    // contador = contador + 1 (seria a mesma coisa)
+let i = 1;
+while(i <= 5){
+    console.log("Valor do i: + "+i);
+    i++; // incremento
+    // i = i + 1 (seria a mesma coisa)
 }
 
+console.log("\nExemplo 2: FOR (para)");
+
+/* for( inicialização da variável; condição, atualização ) */
+
+for( let j = 1; j <= 10; j++ ){
+    console.log("J vale "+j);   
+}
