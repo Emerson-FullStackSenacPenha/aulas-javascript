d44c82c670a9ff5c2bea690ac49e6a34d5f31f8a
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-20 13:55:18 -0300

    Loops

---
+++ js/07-loops.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* 
+Loops ou estruturas de repetição são comandos que, baseados em alguma condição lógica, são programados para executar ações repetidas vezes.
+
+Comandos mais tradicionais: while (Enquanto) e for (Para)
+
+Normalmente, os loops são controlados através de uma variável contadora.
+*/
