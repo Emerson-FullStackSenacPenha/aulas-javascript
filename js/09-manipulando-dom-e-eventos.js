d1a4e0245c7372f689c1e8cad98f93b55c8e7531
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-26 17:25:55 -0300

    lincando Dom Html

--- js/09-manipulando-dom-e-eventos.js
+++ js/09-manipulando-dom-e-eventos.js
@@ -1,3 +1,12 @@
 'use strict'
 
-console.log("Teste");
+/* Exemplo 01 */
+
+// Procurando (querySelector) por um elemento chamado exemplo1
+const exemplo01 =  document.querySelector("#exemplo1");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body");
+
+console.log(exemplo1);
+console.log(mensagem01);
+console.log(pagina);
