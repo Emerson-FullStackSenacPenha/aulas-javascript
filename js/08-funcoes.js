30aab568ea5f5a3b8ab0a8666b3bd12da1317518
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-26 14:18:23 -0300

    Comentario sobre funções

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log("Teste");
+/*
+Sobre Funções
+Funções são blocos de códigos responsáveis por executar determinadas tarefas. Para usar estes blocos/funções, é necessário "Chamar/invocar" a função desejada através do seu nome.
+
+Vantagens ao usar funções:
+
+- Reutilização de códigos
+- Organização de código-fonte, permitindo a separação de diferentes lógicas e responsabilidades
+- Performace melhor
+- Maior facilidade para manutenção
+*/
