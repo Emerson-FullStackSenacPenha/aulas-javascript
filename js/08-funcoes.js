c564e31abcd08b0532fdbd3c7dfeeacdefb3963f
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-26 14:59:25 -0300

    funções exemplos

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -10,4 +10,59 @@ Vantagens ao usar funções:
 - Organização de código-fonte, permitindo a separação de diferentes lógicas e responsabilidades
 - Performace melhor
 - Maior facilidade para manutenção
+
+No JavaScript, existem várias maneiras de se criar uma função:
+
+- Função anônima
+- Função nomeada/declarada
+- Arrow/Function
 */
+
+console.log("Exemplo 1: Função anônima");
+console.log("\n");
+
+const exemplo1 = function(){
+    // corpo da função: ação que a função vai fazer
+    console.log("Olá, função anônima!");
+};
+
+exemplo1();
+
+console.log(" \nExemplo 2: Função Nomeada/Declarada ");
+function exemplo2(){
+    console.log("Está é uma função nomeada!");
+}
+
+exemplo2();
+
+console.log("\nExemplo 3: Arrow Function (Função Flecha/Seta)");
+
+
+/*
+Sintaxe potencialmente mais simples para funções no JavaSxcript
+*/
+
+const exemplo3 = () => {
+    console.log("Sintaxe Arrow Function!");
+};
+
+exemplo3();
+
+/*
+Obs: Funções (em qualquer sintaxe) também podem trabalhar com parâmetros/argumentos.
+
+Quando uma função precisa de valores/dados para algum tipo de processamento, ela recebe valores/dados através de parâmetros/argumentos entre parênteses.
+
+Geralmente, ao terminar o processamento dos dados, a função "retorna para fora do resultado."
+*/
+
+console.log("\nExemplo 4: Função com parâmetro");
+
+function saudacao( nome = "Visitante" ){
+    console.log("Olá, "+nome);
+}
+
+saudacao("Emerson");
+saudacao("Mônica");
+saudacao("Catarina");
+saudacao(); // Neste caso, é usado "Visitante" como valor
