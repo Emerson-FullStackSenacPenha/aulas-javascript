ab529e9ae081c22eab552af28b5fa946bee50813
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-08-27 14:02:03 -0300

    Seletores Js

--- js/09-manipulando-dom-e-eventos.js
+++ js/09-manipulando-dom-e-eventos.js
@@ -3,10 +3,17 @@
 /* Exemplo 01 */
 
 // Procurando (querySelector) por um elemento chamado exemplo1
-const exemplo01 =  document.querySelector("#exemplo1");
-const mensagem01 = document.querySelector("#mensagem01");
-const pagina = document.querySelector("body");
+const exemplo01 =  document.querySelectorAll("#exemplo1");
+const mensagem01 = document.querySelectorAll("#mensagem01");
+const pagina = document.querySelectorAll("body");
 
 console.log(exemplo1);
 console.log(mensagem01);
 console.log(pagina);
+
+/*
+Para selecionar vários elementos, use querySelectorAll. O resultado é uma esp´[ecie de array de elementos (NodeList).
+*/
+
+const paragrafo = document.querySelectorAll("p");
+console.log(paragrafo);
