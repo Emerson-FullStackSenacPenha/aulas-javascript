4601213d69bf3de5d155a177331f1d6ea3c699f3
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-09-03 16:30:57 -0300

    manipulando senha

--- js/10-senha.js
+++ js/10-senha.js
@@ -1,3 +1,26 @@
 'use strict'
 
+const campoSenha = document.querySelector("#senha");
+const botaoMostrar = document.querySelector("#mostrar");
 
+/*
+Monitoramos o evento pointerdown/pointerup pois ele funcionará tanto para o acionamento com mouse no desktop quanto com o dedo em mobile
+*/
+
+
+botaoMostrar.addEventListener("pointerdown", function(){
+    // Tá "Segurando" o botão? Troca o type para text e mostra senha
+    campoSenha.type = "text";
+});
+
+botaoMostrar.addEventListener("pointerup", function(){
+    // Soltou o botão ? Volta type para password
+    campoSenha.type = "password";
+
+});
+
+botaoMostrar.addEventListener("pointerout", function(){
+    // Moveu o botão ? Volta type para password
+    campoSenha.type = "password";
+
+});
